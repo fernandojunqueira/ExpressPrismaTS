7f29029bac1b89a8bab190b0b0a881181799c36a
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-05-16 14:02:19 -0300

    feat: Add global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express'
 import cors from "cors"
 import { globalRoutes } from "./routes"
+import handleError from './errors/handleError'
 
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(globalRoutes)
+app.use(handleError)
 
 export default app
